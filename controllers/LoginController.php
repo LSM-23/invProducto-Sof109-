34e047e3f4353b4e4c3d37f6d436d26b7c757317
LSM-23 <201829744+LSM-23@users.noreply.github.com>
2026-03-20 18:29:50 +0000

    Agregando logica de password hash

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -4,10 +4,16 @@ session_start();
 
 // Recibiendo datos del formulario de login
 $usuario = $_POST['usuario'] ?? '';
-$password = $_POST['password'] ?? '';
+$password_ingresada = $_POST['password'] ?? '';
+//------------------------------------------------------------------------------------
+//Preparando hash de la contraseña (en un caso real, esto buscaría en la base de datos)
+//-------------------------------------------------------------------------------------
+
+$password_hash_admin = password_hash($password_ingresada, PASSWORD_DEFAULT);
+$password_hash_user = password_hash($password_ingresada, PASSWORD_DEFAULT);
 
 // Simulamos la verificación
-if ($usuario === 'admin' && $password === '1234') {
+if ($usuario === 'admin' && password_verify($password_ingresada, $password_hash_admin)) {
     
     // Guardamos los datos en la sesión del servidor de forma segura
     $_SESSION['usuario'] = 'Administrador';
@@ -17,7 +23,7 @@ if ($usuario === 'admin' && $password === '1234') {
     header('Location: ../views/dashboard.php');
     exit;
 
-} else if ($usuario === 'cliente' && $password === '1234') {
+} else if ($usuario === 'cliente' && password_verify($password_ingresada, $password_hash_user)) {
     
     $_SESSION['usuario'] = 'Cliente Invitado';
     $_SESSION['rol'] = 'cliente';
